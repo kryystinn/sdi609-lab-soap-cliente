c9f0a5570024f029084500cd2401a60119ac732d
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-05-02 22:50:54 +0200

    SDI-SW-11.2 Servicio SOAP Cliente

---
+++ src/main/java/com/uniovi/Sdi609LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi609LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi609LabSoapClienteApplication.class, args);
+	}
+
+}
